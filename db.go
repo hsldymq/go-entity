d63b7f96797ebfb65dff677d8329a410687ba30a
hsldymq <hsldymq@qq.com>
2019-11-25 18:37:59 +0800

    refactor db

--- db.go
+++ db.go
@@ -3,9 +3,12 @@ package entity
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"strings"
 
+	"github.com/doug-martin/goqu/v9"
+	_ "github.com/doug-martin/goqu/v9/dialect/mysql"    // 载入 goqu mysql驱动
+	_ "github.com/doug-martin/goqu/v9/dialect/postgres" // 载入 goqu postgres驱动
+	_ "github.com/doug-martin/goqu/v9/dialect/sqlite3"  // 载入 goqu sqlite3驱动
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 )
@@ -15,8 +18,6 @@ var (
 	insertStatements = map[string]string{}
 	updateStatements = map[string]string{}
 	deleteStatements = map[string]string{}
-
-	dialects = map[string]*dialect{}
 )
 
 // DB 数据库接口
@@ -38,68 +39,22 @@ type DB interface {
 	BindNamed(string, interface{}) (string, []interface{}, error)
 }
 
-// dialect 数据库特性
-type dialect struct {
-	Driver    string
-	Returning bool
-}
-
-func getDialect(db DB) *dialect {
-	driver := db.DriverName()
-
-	if v, ok := dialects[driver]; ok {
-		return v
-	}
-
-	dia := &dialect{Driver: driver}
-	if isPostgres(driver) {
-		dia.Returning = true
-	}
-	dialects[dia.Driver] = dia
-
-	return dia
-}
-
-// Load 从数据库载入entity
-func Load(ctx context.Context, entity Entity, db DB) error {
-	ctx, cancel := context.WithTimeout(ctx, ReadTimeout)
-	defer cancel()
-
-	cv, cacheable := entity.(Cacheable)
-	if cacheable {
-		if loaded, err := loadCache(cv); err != nil {
-			return errors.WithMessage(err, "load entity from cache")
-		} else if loaded {
-			return nil
-		}
-	}
-
-	if err := load(ctx, entity, db); err != nil {
-		return errors.WithMessage(err, "load entity from db")
-	}
-
-	if cacheable {
-		if err := SaveCache(cv); err != nil {
-			return errors.WithMessage(err, "found entity")
-		}
-	}
-
-	return nil
-}
-
-func load(ctx context.Context, entity Entity, db DB) error {
-	md, err := getMetadata(entity)
+func doLoad(ctx context.Context, ent Entity, db DB) error {
+	md, err := getMetadata(ent)
 	if err != nil {
 		return err
 	}
 
 	stmt, ok := selectStatements[md.ID]
 	if !ok {
-		stmt = selectStatement(entity, md, getDialect(db))
+		stmt, err = selectStatement(ent, md, db.DriverName())
+		if err != nil {
+			return err
+		}
 		selectStatements[md.ID] = stmt
 	}
 
-	rows, err := sqlx.NamedQueryContext(ctx, db, stmt, entity)
+	rows, err := sqlx.NamedQueryContext(ctx, db, stmt, ent)
 	if err != nil {
 		return errors.WithStack(err)
 	}
@@ -109,50 +64,30 @@ func load(ctx context.Context, entity Entity, db DB) error {
 		return errors.WithStack(sql.ErrNoRows)
 	}
 
-	if err := rows.StructScan(entity); err != nil {
+	if err := rows.StructScan(ent); err != nil {
 		return errors.WithStack(err)
 	}
 
 	return errors.WithStack(rows.Err())
 }
 
-// Insert 插入新entity
-func Insert(ctx context.Context, entity Entity, db DB) (int64, error) {
-	ctx, cancel := context.WithTimeout(ctx, WriteTimeout)
-	defer cancel()
-
-	if err := entity.OnEntityEvent(ctx, EventBeforeInsert); err != nil {
-		return 0, errors.WithMessage(err, "before insert entity")
-	}
-
-	lastID, err := insert(ctx, entity, db)
-	if err != nil {
-		return 0, errors.WithMessage(err, "insert entity")
-	}
-
-	if err := entity.OnEntityEvent(ctx, EventAfterInsert); err != nil {
-		return 0, errors.WithMessage(err, "after insert entity")
-	}
-
-	return lastID, nil
-}
-
-func insert(ctx context.Context, entity Entity, db DB) (int64, error) {
-	md, err := getMetadata(entity)
+func doInsert(ctx context.Context, ent Entity, db DB) (int64, error) {
+	md, err := getMetadata(ent)
 	if err != nil {
 		return 0, err
 	}
 
-	dia := getDialect(db)
-
 	stmt, ok := insertStatements[md.ID]
 	if !ok {
-		stmt = insertStatement(entity, md, dia)
+		stmt, err = insertStatement(ent, md, db.DriverName())
+		if err != nil {
+			return 0, err
+		}
 		insertStatements[md.ID] = stmt
 	}
 
-	if dia.Returning && strings.Contains(stmt, ") RETURNING ") {
-		rows, err := sqlx.NamedQueryContext(ctx, db, stmt, entity)
+	if md.hasReturningInsert {
+		rows, err := sqlx.NamedQueryContext(ctx, db, stmt, ent)
 		if err != nil {
 			return 0, errors.WithStack(err)
 		}
@@ -162,19 +97,19 @@ func insert(ctx context.Context, entity Entity, db DB) (int64, error) {
 			return 0, errors.WithStack(sql.ErrNoRows)
 		}
 
-		if err := rows.StructScan(entity); err != nil {
+		if err := rows.StructScan(ent); err != nil {
 			return 0, errors.WithStack(err)
 		}
 
 		return 0, errors.WithStack(rows.Err())
 	}
 
-	result, err := db.NamedExecContext(ctx, stmt, entity)
+	result, err := db.NamedExecContext(ctx, stmt, ent)
 	if err != nil {
 		return 0, errors.WithStack(err)
 	}
 
-	// postgresql不支持LastInsertId特性
+	// postgres不支持LastInsertId特性
 	if isPostgres(db.DriverName()) {
 		return 0, nil
 	}
@@ -183,47 +118,23 @@ func insert(ctx context.Context, entity Entity, db DB) (int64, error) {
 	return lastID, errors.WithStack(err)
 }
 
-// Update 更新entity
-func Update(ctx context.Context, entity Entity, db DB) error {
-	ctx, cancel := context.WithTimeout(ctx, WriteTimeout)
-	defer cancel()
-
-	if err := entity.OnEntityEvent(ctx, EventBeforeUpdate); err != nil {
-		return errors.WithMessage(err, "before update entity")
-	}
-
-	if err := update(ctx, entity, db); err != nil {
-		return errors.WithMessage(err, "update entity")
-	}
-
-	if v, ok := entity.(Cacheable); ok {
-		if err := DeleteCache(v); err != nil {
-			return errors.WithMessage(err, "after update entity")
-		}
-	}
-
-	return errors.WithMessage(
-		entity.OnEntityEvent(ctx, EventAfterUpdate),
-		"after update entity",
-	)
-}
-
-func update(ctx context.Context, entity Entity, db DB) error {
-	md, err := getMetadata(entity)
+func doUpdate(ctx context.Context, ent Entity, db DB) error {
+	md, err := getMetadata(ent)
 	if err != nil {
 		return err
 	}
 
-	dia := getDialect(db)
-
 	stmt, ok := updateStatements[md.ID]
 	if !ok {
-		stmt = updateStatement(entity, md, dia)
+		stmt, err = updateStatement(ent, md, db.DriverName())
+		if err != nil {
+			return err
+		}
 		updateStatements[md.ID] = stmt
 	}
 
-	if dia.Returning && strings.Contains(stmt, " RETURNING ") {
-		rows, err := sqlx.NamedQueryContext(ctx, db, stmt, entity)
+	if md.hasReturningUpdate {
+		rows, err := sqlx.NamedQueryContext(ctx, db, stmt, ent)
 		if err != nil {
 			return errors.WithStack(err)
 		}
@@ -233,14 +144,14 @@ func update(ctx context.Context, entity Entity, db DB) error {
 			return errors.WithStack(sql.ErrNoRows)
 		}
 
-		if err := rows.StructScan(entity); err != nil {
+		if err := rows.StructScan(ent); err != nil {
 			return errors.WithStack(err)
 		}
 
 		return errors.WithStack(rows.Err())
 	}
 
-	result, err := db.NamedExecContext(ctx, stmt, entity)
+	result, err := db.NamedExecContext(ctx, stmt, ent)
 	if err != nil {
 		return errors.WithStack(err)
 	}
@@ -254,162 +165,156 @@ func update(ctx context.Context, entity Entity, db DB) error {
 	return nil
 }
 
-// Delete 删除entity
-func Delete(ctx context.Context, entity Entity, db DB) error {
-	ctx, cancel := context.WithTimeout(ctx, WriteTimeout)
-	defer cancel()
-
-	if err := entity.OnEntityEvent(ctx, EventBeforeDelete); err != nil {
-		return err
-	}
-
-	if err := _delete(ctx, entity, db); err != nil {
-		return err
-	}
-
-	if v, ok := entity.(Cacheable); ok {
-		if err := DeleteCache(v); err != nil {
-			return errors.WithMessage(err, "after delete entity")
-		}
-	}
-
-	return errors.WithMessage(
-		entity.OnEntityEvent(ctx, EventAfterDelete),
-		"after delete entity",
-	)
-}
-
-func _delete(ctx context.Context, entity Entity, db DB) error {
-	md, err := getMetadata(entity)
+func doDelete(ctx context.Context, ent Entity, db DB) error {
+	md, err := getMetadata(ent)
 	if err != nil {
 		return errors.WithMessage(err, "delete entity")
 	}
 
 	stmt, ok := deleteStatements[md.ID]
 	if !ok {
-		stmt = deleteStatement(entity, md, getDialect(db))
+		stmt, err = deleteStatement(ent, md, db.DriverName())
+		if err != nil {
+			return err
+		}
 		deleteStatements[md.ID] = stmt
 	}
 
-	_, err = db.NamedExecContext(ctx, stmt, entity)
+	_, err = db.NamedExecContext(ctx, stmt, ent)
 	return errors.Wrapf(err, "delete entity %s", md.ID)
 }
 
-func selectStatement(entity Entity, md *Metadata, dia *dialect) string {
-	columns := []string{}
+func selectStatement(ent Entity, md *Metadata, driver string) (string, error) {
+	columns := make([]interface{}, 0, len(md.Columns))
 	for _, col := range md.Columns {
-		columns = append(columns, quoteColumn(col.DBField, dia))
+		columns = append(columns, col.DBField)
 	}
-	stmt := fmt.Sprintf("SELECT %s FROM %s WHERE", strings.Join(columns, ", "), md.TableName)
 
-	for i, col := range md.PrimaryKeys {
-		if i == 0 {
-			stmt += fmt.Sprintf(" %s = :%s", quoteColumn(col.DBField, dia), col.DBField)
-		} else {
-			stmt += fmt.Sprintf(" AND %s = :%s", quoteColumn(col.DBField, dia), col.DBField)
-		}
+	stmt := goqu.Dialect(driverName(driver)).
+		From(md.TableName).
+		Prepared(false).
+		Select(columns...).
+		Limit(1)
+
+	where := goqu.Ex{}
+	for _, col := range md.PrimaryKeys {
+		where[col.DBField] = goqu.L(":" + col.DBField)
 	}
-	stmt += " LIMIT 1"
+	stmt = stmt.Where(where)
+	sqlStr, _, err := stmt.ToSQL()
 
-	return stmt
+	return sqlStr, errors.WithStack(err)
 }
 
-func insertStatement(entity Entity, md *Metadata, dia *dialect) string {
-	columns := []string{}
-	returnings := []string{}
-	placeholder := []string{}
+func insertStatement(ent Entity, md *Metadata, driver string) (string, error) {
+	var columns []interface{}
+	var returning []interface{}
+	var valuePlaceholder []interface{}
 
 	for _, col := range md.Columns {
-		if col.AutoIncrement {
-			continue
-		}
-
-		c := quoteColumn(col.DBField, dia)
-		if dia.Returning && col.Returning {
-			returnings = append(returnings, c)
-		} else {
-			columns = append(columns, c)
-			placeholder = append(placeholder, fmt.Sprintf(":%s", col.DBField))
+		if col.ReturningInsert {
+			returning = append(returning, col.DBField)
+		} else if !col.AutoIncrement {
+			columns = append(columns, col.DBField)
+			valuePlaceholder = append(valuePlaceholder, goqu.L(":"+col.DBField))
 		}
 	}
 
-	stmt := fmt.Sprintf(
-		"INSERT INTO %s (%s) VALUES (%s)",
-		md.TableName,
-		strings.Join(columns, ", "),
-		strings.Join(placeholder, ", "),
-	)
+	stmt := goqu.Dialect(driverName(driver)).
+		Insert(md.TableName).
+		Prepared(false).
+		Cols(columns...).
+		Vals(valuePlaceholder)
 
-	if dia.Returning && len(returnings) > 0 {
-		stmt += fmt.Sprintf(" RETURNING %s", strings.Join(returnings, ", "))
+	if len(returning) > 0 {
+		stmt = stmt.Returning(returning...)
 	}
 
-	return stmt
+	sqlStr, _, err := stmt.ToSQL()
+
+	return sqlStr, errors.WithStack(err)
 }
 
-func updateStatement(entity Entity, md *Metadata, dia *dialect) string {
-	returnings := []string{}
-	stmt := fmt.Sprintf("UPDATE %s SET", md.TableName)
+func updateStatement(ent Entity, md *Metadata, driver string) (string, error) {
+	var returning []interface{}
 
-	set := false
+	stmt := goqu.Dialect(driverName(driver)).
+		Update(md.TableName).
+		Prepared(false)
+
+	set := goqu.Ex{}
 	for _, col := range md.Columns {
-		if col.RefuseUpdate {
-			continue
-		}
-
-		if dia.Returning && col.Returning {
-			returnings = append(returnings, quoteColumn(col.DBField, dia))
-		} else {
-			if set {
-				stmt += fmt.Sprintf(", %s = :%s", quoteColumn(col.DBField, dia), col.DBField)
-			} else {
-				stmt += fmt.Sprintf(" %s = :%s", quoteColumn(col.DBField, dia), col.DBField)
-				set = true
-			}
+		if col.ReturningUpdate {
+			returning = append(returning, col.DBField)
+		} else if !col.RefuseUpdate {
+			set[col.DBField] = goqu.L(":" + col.DBField)
 		}
 	}
+	stmt = stmt.Set(set)
 
-	for i, col := range md.PrimaryKeys {
-		if i == 0 {
-			stmt += fmt.Sprintf(" WHERE %s = :%s", quoteColumn(col.DBField, dia), col.DBField)
-		} else {
-			stmt += fmt.Sprintf(" AND %s = :%s", quoteColumn(col.DBField, dia), col.DBField)
-		}
+	where := goqu.Ex{}
+	for _, col := range md.PrimaryKeys {
+		where[col.DBField] = goqu.L(":" + col.DBField)
+	}
+	stmt = stmt.Where(where)
+
+	if len(returning) > 0 {
+		stmt = stmt.Returning(returning...)
 	}
 
-	if dia.Returning && len(returnings) > 0 {
-		stmt += fmt.Sprintf(" RETURNING %s", strings.Join(returnings, ", "))
-	}
+	sqlStr, _, err := stmt.ToSQL()
 
-	return stmt
+	return sqlStr, errors.WithStack(err)
 }
 
-func deleteStatement(entity Entity, md *Metadata, dia *dialect) string {
-	stmt := fmt.Sprintf("DELETE FROM %s WHERE", md.TableName)
-	for i, col := range md.PrimaryKeys {
-		if i == 0 {
-			stmt += fmt.Sprintf(" %s = :%s", quoteColumn(col.DBField, dia), col.DBField)
-		} else {
-			stmt += fmt.Sprintf(" AND %s = :%s", quoteColumn(col.DBField, dia), col.DBField)
-		}
-	}
+func deleteStatement(ent Entity, md *Metadata, driver string) (string, error) {
+	stmt := goqu.Dialect(driverName(driver)).
+		Delete(md.TableName).
+		Prepared(false)
 
-	return stmt
+	where := goqu.Ex{}
+	for _, col := range md.PrimaryKeys {
+		where[col.DBField] = goqu.L(":" + col.DBField)
+	}
+	stmt = stmt.Where(where)
+	sqlStr, _, err := stmt.ToSQL()
+
+	return sqlStr, errors.WithStack(err)
 }
 
-func quoteColumn(name string, dia *dialect) string {
-	dn := dia.Driver
-	if isPostgres(dn) {
-		return fmt.Sprintf("%q", name)
+// isConflictError 给定的错误是否是唯一性约束错误
+func isConflictError(driver string, err error) bool {
+	s := errors.Cause(err).Error()
+	if isPostgres(driver) {
+		return strings.Contains(s, "duplicate key value violates unique constraint")
+	} else if isMySQL(driver) {
+		return strings.Contains(s, "Duplicate entry")
+	} else if isSQLite3(driver) {
+		return strings.Contains(s, "UNIQUE constraint failed")
 	}
-
-	return fmt.Sprintf("`%s`", name)
-}
-
-func isPostgres(driver string) bool {
-	if driver == "pgx" || driver == "postgres" {
-		return true
-	}
-
 	return false
+}
+
+// isPostgres 给定driver是否是postgres
+func isPostgres(driver string) bool {
+	return driverName(driver) == "postgres"
+}
+
+// isPostgres 给定driver是否是mysql
+func isMySQL(driver string) bool {
+	return driverName(driver) == "mysql"
+}
+
+// isPostgres 给定driver是否是sqlite3
+func isSQLite3(driver string) bool {
+	return driverName(driver) == "sqlite3"
+}
+
+// driverName 驱动名称
+func driverName(driver string) string {
+	if driver == "pgx" {
+		return "postgres"
+	}
+
+	return driver
 }
--- db_test.go
+++ db_test.go
@@ -1,104 +1,49 @@
 package entity
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestStatement(t *testing.T) {
 	t.Run("select", func(t *testing.T) {
 		md, _ := NewMetadata(&GenernalEntity{})
 
-		dia := &dialect{Driver: "mysql", Returning: false}
-		stmt := selectStatement(&GenernalEntity{}, md, dia)
-		expected := "SELECT `id`, `id2`, `name`, `create_at`, `version` FROM genernal WHERE `id` = :id AND `id2` = :id2 LIMIT 1"
-		if stmt != expected {
-			t.Fatalf("GenernalEntity, Expected=%s, Actual=%s", expected, stmt)
-		}
+		_, err := selectStatement(&GenernalEntity{}, md, "mysql")
+		require.NoError(t, err)
 
-		dia = &dialect{Driver: "postgres", Returning: true}
-		stmt = selectStatement(&GenernalEntity{}, md, dia)
-		expected = `SELECT "id", "id2", "name", "create_at", "version" FROM genernal WHERE "id" = :id AND "id2" = :id2 LIMIT 1`
-		if stmt != expected {
-			t.Fatalf("GenernalEntity, Expected=%s, Actual=%s", expected, stmt)
-		}
+		_, err = selectStatement(&GenernalEntity{}, md, "postgres")
+		require.NoError(t, err)
 	})
 
 	t.Run("insert", func(t *testing.T) {
 		md, _ := NewMetadata(&GenernalEntity{})
 
-		dia := &dialect{Driver: "mysql", Returning: false}
-		stmt := insertStatement(&GenernalEntity{}, md, dia)
-		expected := "INSERT INTO genernal (`id2`, `name`, `create_at`, `version`) VALUES (:id2, :name, :create_at, :version)"
-		if stmt != expected {
-			t.Fatalf("GenernalEntity, Expected=%s, Actual=%s", expected, stmt)
-		}
+		_, err := insertStatement(&GenernalEntity{}, md, "mysql")
+		require.Error(t, err) // MySQL不支持returning
 
-		dia = &dialect{Driver: "postgres", Returning: true}
-		stmt = insertStatement(&GenernalEntity{}, md, dia)
-		expected = `INSERT INTO genernal ("id2", "name", "create_at") VALUES (:id2, :name, :create_at) RETURNING "version"`
-		if stmt != expected {
-			t.Fatalf("GenernalEntity, Expected=%s, Actual=%s", expected, stmt)
-		}
+		_, err = insertStatement(&GenernalEntity{}, md, "postgres")
+		require.NoError(t, err)
 	})
 
 	t.Run("update", func(t *testing.T) {
 		md, _ := NewMetadata(&GenernalEntity{})
 
-		dia := &dialect{Driver: "mysql", Returning: false}
-		stmt := updateStatement(&GenernalEntity{}, md, dia)
-		expected := "UPDATE genernal SET `name` = :name, `version` = :version WHERE `id` = :id AND `id2` = :id2"
-		if stmt != expected {
-			t.Fatalf("GenernalEntity, Expected=%s, Actual=%s", expected, stmt)
-		}
+		_, err := updateStatement(&GenernalEntity{}, md, "mysql")
+		require.Error(t, err) // MySQL不支持returning
 
-		dia = &dialect{Driver: "postgres", Returning: true}
-		stmt = updateStatement(&GenernalEntity{}, md, dia)
-		expected = `UPDATE genernal SET "name" = :name WHERE "id" = :id AND "id2" = :id2 RETURNING "version"`
-		if stmt != expected {
-			t.Fatalf("GenernalEntity, Expected=%s, Actual=%s", expected, stmt)
-		}
+		_, err = updateStatement(&GenernalEntity{}, md, "postgres")
+		require.NoError(t, err)
 	})
 
 	t.Run("delete", func(t *testing.T) {
 		md, _ := NewMetadata(&GenernalEntity{})
 
-		dia := &dialect{Driver: "mysql", Returning: false}
-		stmt := deleteStatement(&GenernalEntity{}, md, dia)
-		expected := "DELETE FROM genernal WHERE `id` = :id AND `id2` = :id2"
-		if stmt != expected {
-			t.Fatalf("GenernalEntity, Expected=%s, Actual=%s", expected, stmt)
-		}
+		_, err := deleteStatement(&GenernalEntity{}, md, "mysql")
+		require.NoError(t, err)
 
-		dia = &dialect{Driver: "postgres", Returning: true}
-		stmt = deleteStatement(&GenernalEntity{}, md, dia)
-		expected = `DELETE FROM genernal WHERE "id" = :id AND "id2" = :id2`
-		if stmt != expected {
-			t.Fatalf("GenernalEntity, Expected=%s, Actual=%s", expected, stmt)
-		}
+		_, err = deleteStatement(&GenernalEntity{}, md, "postgres")
+		require.NoError(t, err)
 	})
-}
-
-func TestQuoteColumn(t *testing.T) {
-	tests := []struct {
-		driver   string
-		column   string
-		expected string
-	}{
-		{
-			driver:   "mysql",
-			column:   "id",
-			expected: "`id`",
-		},
-		{
-			driver:   "postgres",
-			column:   "id",
-			expected: `"id"`,
-		},
-	}
-
-	for _, test := range tests {
-		dia := &dialect{Driver: test.driver}
-
-		if actual := quoteColumn(test.column, dia); actual != test.expected {
-			t.Fatalf("%q quote column, Expected=%v, Actual=%v", test.driver, test.expected, actual)
-		}
-	}
 }
